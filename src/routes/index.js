c47aa4946c28c2d93538a9848a5f6cb44c84ef81
chunza2542 <chunza2542@hotmail.com>
2018-08-12 11:28:27 +0700

    remove session and affiliates

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import users from "./users"
 // import count from './count';
 import admin from "./admin"
 // import questions from './questions';
-import affiliates from "./affiliates"
 import auth from "./auth"
 // import member from './member';
 import registration from "./registration"
@@ -20,7 +19,6 @@ router.use("/users", users)
 // router.use('/count', count);
 router.use("/admin", admin)
 // router.use('/questions', questions);
-router.use("/affiliates", affiliates)
 router.use("/auth", auth)
 // router.use('/slip', slip);
 // router.use('/member', member);

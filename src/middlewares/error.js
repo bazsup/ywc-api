d5e7fbf8956d891a3fe892e35c16f082074a41ba
chunza2542 <chunza2542@hotmail.com>
2018-08-12 14:53:51 +0700

    store error message on file using winston

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,6 +1,28 @@
+import winston from "winston"
+
 import {createJsonResponse} from "../utils"
 
+const logger = winston.createLogger({
+  level: "error",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ]
+})
+
 export const errorHandler = (err, req, res, next) => {
+  // log message to console
+  console.error("Error", err.message)
+
+  // store error message to file on production server
+  if (process.env.NODE_ENV === "production") {
+    logger.log({
+      level: "error",
+      message: err.message,
+    })
+  }
+
+  // response error with json
   res.status(500)
   res.json(createJsonResponse("error", {
     message: err.message,

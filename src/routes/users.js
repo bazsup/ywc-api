fdbff9095d4cc56daa13ebe49a22669773917564
chunza2542 <chunza2542@hotmail.com>
2018-10-19 23:30:21 +0700

    create route /users/stat/all

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ import {pick} from "lodash"
 import VError from "verror"
 
 import User from "../models/user"
-import {ROLE_STAFF, ROLE_COMMITTEE, ROLE_COMPLETED} from "../utils/const"
+import {ROLE_STAFF, ROLE_COMMITTEE, ROLE_COMPLETED, ROLE_MANAGER} from "../utils/const"
 import {createJsonResponse} from "../utils/helpers"
 import {authen, adminAuthen} from "../middlewares/authenticator"
 
@@ -171,160 +171,46 @@ router.get("/stat", async (req, res) => {
   }
 })
 
-// router.get("/by-day-stat", adminAuthen("admin"), async (req, res) => {
-//   try {
-//     const {sort = "desc"} = req.query
-//     const statistics = await User.aggregate([
-//       {$match: {status: "completed"}},
-//       {$sort: {completed_at: 1}},
-//       {
-//         $project: {
-//           dateString: {
-//             $dateToString: {format: "%Y-%m-%d", date: "$completed_at"},
-//           },
-//         },
-//       },
-//       {$group: {_id: "$dateString", count: {$sum: 1}}},
-//     ])
-//     return res.send(
-//       statistics.sort((a, b) => {
-//         if (moment(a._id, "YYYY-MM-DD").isBefore(moment(b._id, "YYYY-MM-DD")))
-//           return sort === "desc" ? 1 : -1
-//         else if (
-//           moment(a._id, "YYYY-MM-DD").isSame(moment(b._id, "YYYY-MM-DD"))
-//         )
-//           return 0
-//         return sort === "desc" ? -1 : 1
-//       }),
-//     )
-//   } catch (e) {
-//     return res.error(e)
-//   }
-// })
+// backoffice candidates stat (dashboard)
+router.get(
+  "/stat/all",
+  adminAuthen([ROLE_COMMITTEE, ROLE_MANAGER]),
+  async (req, res, next) => {
+    try {
+      const countUserStep = await User.aggregate(
+        [
+          {
+             $addFields: {
+                step_info: {$ne: [{$ifNull: ["$firstName", false]}, false]},
+                step_contact: {$ne: [{$ifNull: ["$phone", false]}, false]},
+                step_insight: {$ne: [{$ifNull: ["$activities", false]}, false]}
+             }
+          },
+          {
+             $addFields: {
+                step_major: {$and: [ {$eq: ["$step_info", true] }, {$eq: ["$step_contact", true] }, {$eq: ["$step_insight", true] } ]}
+             }
+          },
+          {
+             $group: {
+                "_id": {
+                   "major": "$major",
+                   "step_info": "$step_info",
+                   "step_contact": "$step_contact",
+                   "step_insight": "$step_insight",
+                   "step_major": "$step_major"
+                },
+                "userCount": {"$sum": 1}
+             }
+          }
+       ]
+      )
 
-// router.get(
-//   "/programming",
-//   adminAuthen(["admin", "programming"]),
-//   async (req, res) => {
-//     try {
-//       const users = await User.find({status: "completed", major: "programming"})
-//       return res.send(users)
-//     } catch (err) {
-//       return res.error(err)
-//     }
-//   },
-// )
-
-// router.get("/design", adminAuthen(["admin", "design"]), async (req, res) => {
-//   try {
-//     const users = await User.find({status: "completed", major: "design"})
-//     return res.send(users)
-//   } catch (err) {
-//     return res.error(err)
-//   }
-// })
-
-// router.get(
-//   "/marketing",
-//   adminAuthen(["admin", "marketing"]),
-//   async (req, res) => {
-//     try {
-//       const users = await User.find({status: "completed", major: "marketing"})
-//       return res.send(users)
-//     } catch (err) {
-//       return res.error(err)
-//     }
-//   },
-// )
-
-// router.get("/content", adminAuthen(["admin", "content"]), async (req, res) => {
-//   try {
-//     const users = await User.find({status: "completed", major: "content"})
-//     return res.send(users)
-//   } catch (err) {
-//     return res.error(err)
-//   }
-// })
-
-// router.get("/interview", async (req, res) => {
-//   try {
-//     const interviewCandidate = await User.find({
-//       status: "completed",
-//       isPassStageOne: true,
-//       isPassStageTwo: true,
-//       isPassStageThree: true,
-//     })
-//       .select(
-//         "_id major title firstName lastName firstNameEN lastNameEN email nickname completed_at",
-//       )
-//       .sort("firstName")
-//     return res.send({
-//       programming: interviewCandidate.filter(
-//         (user) => user.major === "programming",
-//       ),
-//       design: interviewCandidate.filter((user) => user.major === "design"),
-//       marketing: interviewCandidate.filter(
-//         (user) => user.major === "marketing",
-//       ),
-//       content: interviewCandidate.filter((user) => user.major === "content"),
-//     })
-//   } catch (err) {
-//     return res.error(err)
-//   }
-// })
-
-// router.get("/:id", adminAuthen("admin"), async (req, res) => {
-//   const user = await User.findOne({_id: req.params.id}).populate("questions")
-//   return res.send(user)
-// })
-
-// // check backoffice candidates stat
-// router.get("/stat/all", adminAuthen("admin"), async (req, res) => {
-//   try {
-//     const programmingCompleted = User.count({
-//       status: "completed",
-//       major: "programming",
-//     })
-//     const designCompleted = User.count({status: "completed", major: "design"})
-//     const contentCompleted = User.count({status: "completed", major: "content"})
-//     const marketingCompleted = User.count({
-//       status: "completed",
-//       major: "marketing",
-//     })
-//     const pendingPromise = User.count({
-//       status: {$ne: "completed"},
-//       completed: {$ne: [true, true, true, true, true]},
-//     })
-//     const notConfirmPromise = User.count({
-//       status: {$ne: "completed"},
-//       completed: [true, true, true, true, true],
-//     })
-//     const [
-//       programming,
-//       design,
-//       content,
-//       marketing,
-//       pending,
-//       notConfirm,
-//     ] = await Promise.all([
-//       programmingCompleted,
-//       designCompleted,
-//       contentCompleted,
-//       marketingCompleted,
-//       pendingPromise,
-//       notConfirmPromise,
-//     ])
-//     return res.send({
-//       programming,
-//       design,
-//       content,
-//       marketing,
-//       pending,
-//       notConfirm,
-//     })
-//   } catch (err) {
-//     return res.error(err)
-//   }
-// })
+      return res.send(createJsonResponse("success", {countUserStep}))
+    } catch (e) {
+      return next(new VError(e, "/stat/all"))
+    }
+  },
+)
 
 export default router

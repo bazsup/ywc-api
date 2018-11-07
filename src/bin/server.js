777b37973cd3b123653b1d5919a32f6ea0e5eba1
chunza2542 <chunza2542@hotmail.com>
2018-11-07 21:41:14 +0700

    clean up server.js

--- src/bin/server.js
+++ src/bin/server.js
@@ -4,7 +4,6 @@ import http from "http";
 import https from "https";
 
 import app from "../app";
-import {Queue, Score} from "../models";
 import {Certificate} from "crypto";
 
 require("babel-polyfill");
@@ -12,21 +11,6 @@ require("babel-polyfill");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
-const io = require("socket.io")(server);
-
-const sendCurrentScore = () =>
-  Score.find({}).then(scores => io.emit("score", scores));
-
-io.on("connection", () => {
-  // sendCurrentQueue();
-  sendCurrentScore();
-});
-
-server.on("request", req => {
-  // req.ioSendQueue = sendCurrentQueue;
-  req.ioSendScore = sendCurrentScore;
-});
 
 // run http & https server
 if (process.env.NODE_ENV === "production") {

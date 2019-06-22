82aa3382157407148aabd7b536830117e8f21a45
Wiput Pootong <wiput.pootong@gmail.com>
2019-06-22 20:10:49 +0700

    :heavy_minus_sign: remove unused dependencies

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,5 +1,4 @@
 import http from 'http'
-// import https from "https";
 
 import app from '../app'
 
@@ -8,21 +7,6 @@ require('babel-polyfill')
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app)
-
-// run http & https server
-// if (process.env.NODE_ENV === "production") {
-//   // cloudflare credentials
-//   const credentials = {
-//     key: process.env.PRIVATE_KEY || "",
-//     cert: process.env.CERTIFICATE || "",
-//   };
-
-//   const httpsServer = https.createServer(credentials, app);
-
-//   httpsServer.listen(PORT + 443, () => {
-//     console.log(`Listening https on port ${PORT + 443}`);
-//   });
-// }
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
